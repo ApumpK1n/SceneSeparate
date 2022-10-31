87e74a195db7164cc7b1c6d740827533d76b025f
ApumpK1n <lsmzng@163.com>
2022-11-01 00:10:25 +0800

    Update BoundsEx.cs

--- Assets/SceneSeparate/Scripts/Utils/BoundsEx.cs
+++ Assets/SceneSeparate/Scripts/Utils/BoundsEx.cs
@@ -16,41 +16,43 @@ public static class BoundsEx
     }
 
     /// <summary>
-    /// 判断包围盒是否包含另一个包围盒
+    /// 判断bounds是否在相机内
     /// </summary>
-    /// <param name="bounds"></param>
-    /// <param name="compareTo"></param>
+    /// <param name="bound"></param>
+    /// <param name="camera"></param>
+    /// <param name="viewRatio"></param>
     /// <returns></returns>
-    public static bool IsBoundsContainsAnotherBounds(this Bounds bounds, Bounds compareTo)
+    public static bool CheckBoundIsInCamera(this Bounds bound, Camera camera, float viewRatio = 1)
     {
-        if (
-            !bounds.Contains(compareTo.center +
-                             new Vector3(-compareTo.size.x / 2, compareTo.size.y / 2, -compareTo.size.z / 2)))
-            return false;
-        if (
-            !bounds.Contains(compareTo.center + new Vector3(compareTo.size.x / 2, compareTo.size.y / 2, -compareTo.size.z / 2)))
-            return false;
-        if (!bounds.Contains(compareTo.center + new Vector3(compareTo.size.x / 2, compareTo.size.y / 2, compareTo.size.z / 2)))
-            return false;
-        if (
-            !bounds.Contains(compareTo.center + new Vector3(-compareTo.size.x / 2, compareTo.size.y / 2, compareTo.size.z / 2)))
-            return false;
-        if (
-            !bounds.Contains(compareTo.center +
-                             new Vector3(-compareTo.size.x / 2, -compareTo.size.y / 2, -compareTo.size.z / 2)))
-            return false;
-        if (
-            !bounds.Contains(compareTo.center +
-                             new Vector3(compareTo.size.x / 2, -compareTo.size.y / 2, -compareTo.size.z / 2)))
-            return false;
-        if (
-            !bounds.Contains(compareTo.center + new Vector3(compareTo.size.x / 2, -compareTo.size.y / 2, compareTo.size.z / 2)))
-            return false;
-        if (
-            !bounds.Contains(compareTo.center +
-                             new Vector3(-compareTo.size.x / 2, -compareTo.size.y / 2, compareTo.size.z / 2)))
-            return false;
-        return true;
+        System.Func<Vector4, int> ComputeOutCode = (projectionPos) =>
+        {
+            int _code = 0;
+            if (projectionPos.x < -projectionPos.w) _code |= 1;
+            if (projectionPos.x > projectionPos.w) _code |= 2;
+            if (projectionPos.y < -projectionPos.w) _code |= 4;
+            if (projectionPos.y > projectionPos.w) _code |= 8;
+            if (projectionPos.z < -projectionPos.w) _code |= 16;
+            if (projectionPos.z > projectionPos.w) _code |= 32;
+            return _code;
+        };
+
+        Vector4 worldPos = Vector4.one;
+        int code = 63;
+        for (int i = -1; i <= 1; i += 2)
+        {
+            for (int j = -1; j <= 1; j += 2)
+            {
+                for (int k = -1; k <= 1; k += 2)
+                {
+                    worldPos.x = bound.center.x + i * bound.extents.x;
+                    worldPos.y = bound.center.y + j * bound.extents.y;
+                    worldPos.z = bound.center.z + k * bound.extents.z;
+
+                    code &= ComputeOutCode(camera.projectionMatrix * camera.worldToCameraMatrix * worldPos * viewRatio);
+                }
+            }
+        }
+        return code == 0 ? true : false;
     }
 
     /// <summary>
